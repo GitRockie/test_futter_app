0e22bd04eb98d6cfc2872a3114fcd49103b277fd
GitRockie <elromankos@gmail.com>
2022-12-26 18:43:30 +0100

    Update login_page.dart
    Button add
    SpanText in RichText widget

--- dinbeat_test/lib/login_page.dart
+++ dinbeat_test/lib/login_page.dart
@@ -14,102 +14,140 @@ class _LoginPageState extends State<LoginPage> {
     double h = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(children: [
-        Container(
-          width: w,
-          height: h * 0.3,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('img/loginimg.png'), fit: BoxFit.cover)),
-        ),
-        Container(
-          margin: const EdgeInsets.only(left: 20, right: 20),
-          width: w,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                '¡Hola!',
-                style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
-              ),
-              const Text(
-                'Inicia sesión en tu cuenta',
-                style: TextStyle(fontSize: 20, color: Colors.grey),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 10,
-                          spreadRadius: 7,
-                          offset: const Offset(1, 1),
-                          color: Colors.grey.withOpacity(0.2))
-                    ]),
-                child: TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(
-                              color: Colors.white, width: 1.0)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(
-                              color: Colors.white, width: 1.0)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30))),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 10,
-                          spreadRadius: 7,
-                          offset: const Offset(1, 1),
-                          color: Colors.grey.withOpacity(0.2))
-                    ]),
-                child: TextField(
-                  decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(
-                              color: Colors.white, width: 1.0)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(
-                              color: Colors.white, width: 1.0)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30))),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(),
-                  ),
-                  const Text(
-                    '¿Olvidaste tu contraseña?',
-                    style: TextStyle(fontSize: 20, color: Colors.grey),
-                  ),
-                ],
-              ),
-            ],
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+            width: w,
+            height: h * 0.3,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('img/loginimg.png'), fit: BoxFit.cover)),
           ),
-        )
-      ]),
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            width: w,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  '¡Hola!',
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  'Inicia sesión en tu cuenta',
+                  style: TextStyle(fontSize: 20, color: Colors.grey),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 7,
+                            offset: const Offset(1, 1),
+                            color: Colors.grey.withOpacity(0.2))
+                      ]),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 1.0)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 1.0)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 7,
+                            offset: const Offset(1, 1),
+                            color: Colors.grey.withOpacity(0.2))
+                      ]),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 1.0)),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 1.0)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(),
+                    ),
+                    const Text(
+                      '¿Olvidaste tu contraseña?',
+                      style: TextStyle(fontSize: 20, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Container(
+            width: w * 0.5,
+            height: h * 0.08,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: const DecorationImage(
+                    image: AssetImage('img/loginbtn.png'), fit: BoxFit.cover)),
+            child: const Center(
+              child: Text(
+                'Log in',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: w * 0.04,
+          ),
+          RichText(
+              text: TextSpan(
+                  text: '¿Aún no tienes una cuenta?',
+                  style: TextStyle(color: Colors.grey[500], fontSize: 20),
+                  children: const [
+                TextSpan(
+                    text: ' Crear',
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ))
+              ]))
+        ]),
+      ),
     );
   }
 }
